7fb0df0ab352559265a87a777f76397a93d4f2f3
hyborninc@gmail.com <hyborninc@gmail.com>
2023-04-15 16:57:35 +0100

    inittial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#e9c176",
+        blackish: "#000000",
+        whitish: "#f7e8d0",
+      },
+
+      fontFamily: {
+        sans: ["Black Han Sans", "sans-serif"],
+        mono: ["Poiret One", "monospace"],
+        mini: ["Nunito", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
